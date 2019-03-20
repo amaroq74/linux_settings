662aea810086d07a352d52c39a4fbe737efdab6c
Ryan Herbst <rherbst@slac.stanford.edu>
2019-03-20 00:56:22 -0700

    Updates

--- bashrc_home.sh
+++ bashrc_home.sh
@@ -9,10 +9,10 @@ source ~/.linux_settings/bashrc_common.sh
 
 ##### Aliases ######
 alias aliska='ssh aliska.amaroq.net -X'
-alias achrome='ssh aliska.amaroq.net -X google-chrome-stable'
 alias agqrx='ssh aliska.amaroq.net -X "pax11publish -r; gqrx"'
 alias remaudio='vlc --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=48000 udp://@:7355'
-alias remrig='grig --model=2 -r 172.16.20.12 &'
+alias rgrig='grig --model=2 -r 172.16.20.12 &'
+alias rrigctl='rigctl --model=2 -r 172.16.20.12'
 alias wlink='pat --listen ax25 http &'
 alias aprs='xastir'
 alias pcon='pat connect ax25:///K6ATH-10?freq=145730'

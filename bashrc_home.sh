562a0986aa6de23c56a9e27a2f5a6d3ed868a366
Ryan Herbst <rherbst@slac.stanford.edu>
2019-03-26 20:18:07 -0700

    Updates

--- bashrc_home.sh
+++ bashrc_home.sh
@@ -13,16 +13,18 @@ alias aliska='ssh aliska.amaroq.net -X'
 #### Ham Radio ####
 alias agqrx='ssh aliska.amaroq.net -X "pax11publish -r; gqrx"'
 alias remaudio='vlc --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=48000 udp://@:7355'
+alias laudio='pax11publish -r'
+
 alias rgrig='grig --model=2 -r 172.16.20.12 &'
 alias lgrig='grig --model=2 -r 127.0.0.1 &'
 alias rrigctl='rigctl --model=2 -r 172.16.20.12'
 alias lrigctl='rigctl --model=2 -r 127.0.0.1'
-alias pcon='pat connect ax25:///K6ATH-10?freq=145730'
-alias pweb='pat http'
-alias laudio='pax11publish -r'
+
+alias pcon='rrigctl F 145730000 M PKTFM 0; pat connect ax25:///K6ATH-10'
+alias pweb='rrigctl F 145730000 M PKTFM 0; pat http'
+alias n0ary='rrigctl F 145090000 M PKTFM 0; call usb n0ary-1'
+alias aprs='rrigctl F 144390000 M PKTFM 0; xastir'
+
 alias ax25off='sudo systemctl stop ax25; sudo systemctl stop soundmodem'
 alias ax25on='sudo systemctl restart soundmodem; sudo systemctl restart ax25'
-alias n0ary='rrigctl F 145090000; call usb n0ary-1'
-alias aprs='rrigctl F 144390000; listen'
-alias aprsgui='rrigctl F 144390000; xastir'
 

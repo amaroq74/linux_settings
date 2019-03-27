63a73e727fcca83999d92fcd310c0bb232df3708
Ryan Herbst <rherbst@slac.stanford.edu>
2019-03-26 19:07:58 -0700

    Updates

--- bashrc_home.sh
+++ bashrc_home.sh
@@ -9,12 +9,20 @@ source ~/.linux_settings/bashrc_common.sh
 
 ##### Aliases ######
 alias aliska='ssh aliska.amaroq.net -X'
+
+#### Ham Radio ####
 alias agqrx='ssh aliska.amaroq.net -X "pax11publish -r; gqrx"'
 alias remaudio='vlc --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=48000 udp://@:7355'
 alias rgrig='grig --model=2 -r 172.16.20.12 &'
+alias lgrig='grig --model=2 -r 127.0.0.1 &'
 alias rrigctl='rigctl --model=2 -r 172.16.20.12'
-alias wlink='pat --listen ax25 http &'
-alias aprs='xastir'
+alias lrigctl='rigctl --model=2 -r 127.0.0.1'
 alias pcon='pat connect ax25:///K6ATH-10?freq=145730'
 alias pweb='pat http'
 alias laudio='pax11publish -r'
+alias ax25off='sudo systemctl stop ax25; sudo systemctl stop soundmodem'
+alias ax25on='sudo systemctl restart soundmodem; sudo systemctl restart ax25'
+alias n0ary='rrigctl F 145090000; call usb n0ary-1'
+alias aprs='rrigctl F 144390000; listen'
+alias aprsgui='rrigctl F 144390000; listen'
+

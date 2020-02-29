1640a71cd0966b62afb2b14affb823880a647a34
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-28 21:44:06 -0800

    Updates

--- bashrc_pius.sh
+++ bashrc_pius.sh
@@ -2,7 +2,7 @@
 source ~/.linux_settings/bashrc_common.sh
 
 ##### Variables ######
-export USER_LIB_PATH=$HOME/projects/arduino_libraries/libraries/
+export USER_LIB_PATH=$HOME/arduino_libraries/libraries/
 
 ##### PATH ######
 

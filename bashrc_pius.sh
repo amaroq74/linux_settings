4a5df7b335e314f8d35318af086d3b27544aae64
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-28 21:36:44 -0800

    Added pius

---
+++ bashrc_pius.sh
@@ -0,0 +1,12 @@
+
+source ~/.linux_settings/bashrc_common.sh
+
+##### Variables ######
+export USER_LIB_PATH=$HOME/projects/arduino_libraries/libraries/
+
+##### PATH ######
+
+##### Libraries ######
+
+##### Aliases ######
+

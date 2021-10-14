5b571e8c5013f312ab96b805067b4c72c12d7c8b
Ryan Herbst <rherbst@slac.stanford.edu>
2021-10-13 20:23:34 -0700

    Update settings

--- bashrc_home.sh
+++ bashrc_home.sh
@@ -6,14 +6,8 @@ export USER_LIB_PATH=$HOME/projects/arduino_libraries/libraries/
 
 ##### PATH ######
 
-export PATH=$PATH:/opt/stm32cubeprog/bin/
-
 ##### Libraries ######
 
 ##### Aliases ######
 
 
-alias misc_mount='mkdir $HOME/Desktop/misc; sudo cryptsetup luksOpen $HOME/Documents/Personal/old/misc.iso misc;sudo mount -o ro /dev/mapper/misc $HOME/Desktop/misc'
-alias misc_umount='sudo umount $HOME/Desktop/misc; sudo cryptsetup luksClose misc'
-alias gm_mount='mkdir $HOME/Desktop/gm; sudo cryptsetup luksOpen $HOME/Documents/Personal/old/gm.iso gm;sudo mount -o ro /dev/mapper/gm $HOME/Desktop/gm'
-alias gm_umount='sudo umount $HOME/Desktop/gm; sudo cryptsetup luksClose gm'

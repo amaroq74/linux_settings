ba0db92d475035e51435989146f034d1f0916239
Ryan Herbst <rherbst@slac.stanford.edu>
2022-03-24 11:08:12 -0700

    Updates

--- bashrc_slac.sh
+++ bashrc_slac.sh
@@ -18,6 +18,7 @@ alias afs='kinit rherbst; klog.krb5 rherbst'
 alias vnc='sudo systemctl start x11vnc_rherbst'
 alias vnc2='vncserver :2 &'
 alias jlab='ssh -Y rherbst@hallgw.jlab.org -L 5930:localhost:5930 -L 5931:localhost:5931'
+alias dtc='/afs/slac/g/cci/package/linux-xlnx/scripts/dtc/dtc'
 
 # Simulation
 alias sim='psApp s2017.03; source /afs/slac.stanford.edu/g/reseng/synopsys/vcs-mx/M-2017.03-1/settings.sh'
@@ -35,4 +36,5 @@ alias x2019.1='psApp x2019.1; source /afs/slac/g/reseng/xilinx/vivado_2019.1/set
 alias x2019.2='psApp x2019.2; source /afs/slac.stanford.edu/g/reseng/xilinx/2019.2/Vivado/2019.2/settings64.sh'
 
 alias xil='x2019.1'
+
 

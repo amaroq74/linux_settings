fba67d8ec1711e018dcd444781207e20848060e8
Ryan Herbst <rherbst@slac.stanford.edu>
2023-02-08 17:42:57 -0800

    Updates

--- bashrc_home.sh
+++ bashrc_home.sh
@@ -13,5 +13,3 @@ export USER_LIB_PATH=$HOME/projects/arduino_libraries/libraries/
 
 alias rrail='/home/ryan/Rocrail/rocview.sh /home/ryan/Rocrail'
 
-alias gqrx_20='gqrx --conf /home/ryan/.config/gqrx/sdr_20.conf'
-alias gqrx_30='gqrx --conf /home/ryan/.config/gqrx/sdr_30.conf'

4291dd3e7d996c610a7c3bdeae9cc6a39711d05e
Ryan Herbst <rherbst@slac.stanford.edu>
2024-09-24 21:04:05 -0700

    Updates

--- bashrc_radiopi.sh
+++ bashrc_radiopi.sh
@@ -10,19 +10,20 @@ source ~/.linux_settings/bashrc_home.sh
 ##### Aliases ######
 
 #### Ham Radio ####
-#alias lgrig='grig --model=1022 --speed=4800 -r /dev/ttyAMA0 &'
-#alias lrigctl='rigctl --model=1022 -r /dev/ttyAMA0 --serial-speed=4800'
-
-alias lgrig='grig --model=4 -r 127.0.0.1:12345 &'
 alias lrigctl='rigctl --model=4 -r 127.0.0.1:12345'
 
-alias pcon='lrigctl F 145730000 M PKTFM 0; pat connect ax25:///K6ATH-10'
-alias n0ary='lrigctl F 145090000 M PKTFM 0; axcall sm0 n0ary-1'
-alias aprsen='lrigctl F 144390000 M PKTFM 0'
-alias ardopen='lrigctl F 007102200 M USB 0'
-alias ardop='/usr/local/bin/piardopc 8515 plughw:2,0 plughw:2,0'
-alias modepkt='lrigctl M PKTFM 0'
-alias modeusb='lrigctl M USB 0'
+alias pcon_ke6afe='lrigctl V VFOA F 145630000 M PKTFM 0; pat connect ax25:///KE6AFE-10'
+alias pcon_n6zx='lrigctl V VFOA F 145630000 M PKTFM 0; pat connect ax25:///N6ZX-10'
+
+alias n0ary='lrigctl V VFOA F 145090000 M PKTFM 0; axcall sm0 n0ary-1'
+alias aprsen='lrigctl V VFOA F 144390000 M PKTFM 0'
+
+alias ardopen='lrigctl V VFOA F 007102200 M USB 0'
+alias ardop='/usr/local/bin/piardopc 8515 plughw:3,0 plughw:3,0'
+
+alias modepkt='lrigctl V VFOA M PKTFM 0'
+alias modeusb='lrigctl V VFOA M USB 0'
+
 alias gt='GridTracker --enable-speech-dispatcher'
 
 alias audio='pax11publish -r'

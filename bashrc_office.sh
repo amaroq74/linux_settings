f8790a968b440093312352a2d029459b98563f71
Ryan Herbst <rherbst@slac.stanford.edu>
2025-03-15 22:46:20 -0700

    Updates

--- bashrc_office.sh
+++ bashrc_office.sh
@@ -12,19 +12,19 @@ export USER_LIB_PATH=$HOME/projects/arduino_libraries/libraries/
 ##### Aliases ######
 
 #### Ham Radio ####
-alias lrigctl='rigctl --model=4 -r 127.0.0.1:12346'
-
-alias lrigfreq='lrigctl V VFOB f'
-
 alias pcon_ke6afe='kenwood_mem 32;pat connect ax25:///KE6AFE-10'
 alias pcon_n6zx='kenwood_mem 32;pat connect ax25:///N6ZX-10'
-alias aprsen='kenwood_mem 34'
-alias n0ary='kenwood_mem 31; call ax0 n0ary-1'
-alias w3xsc='kenwood_mem 30; call ax0 w3xsc'
-alias k9jfc='kenwood_mem 43; call ax0 k6fjc-5'
 
-alias gqrx_funcube='gqrx --conf /home/ryan/.config/gqrx/funcube_7536.conf'
-alias gqrx_rtlsdr='gqrx --conf /home/ryan/.config/gqrx/rtl_sdr_7537.conf'
+alias aprsen='kenwood_mem 34'
+
+alias bbs_n0ary='kenwood_mem 31'
+alias bbs_w3xsc='kenwood_mem 30'
+alias bbs_kjohn='kenwood_mem 43'
+
+alias gqrx_fcd0='gqrx --conf /home/ryan/.config/gqrx/funcube_fcd_0.conf'
+alias gqrx_fcd1='gqrx --conf /home/ryan/.config/gqrx/funcube_fcd_1.conf'
+
+alias dpacket='direwolf -c /home/ryan/Documents/HamRadio/direwolf_sdr.conf'
 
 alias yaac='java -jar /usr/local/yaac/YAAC.jar'
 

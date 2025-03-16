4f9dbd3c60cbe6c8728ecf0b3932b30f50a9b35b
Ryan Herbst <rherbst@slac.stanford.edu>
2025-03-15 22:45:52 -0700

    Updates

--- bashrc_radiopi.sh
+++ bashrc_radiopi.sh
@@ -15,18 +15,24 @@ alias lrigctl='rigctl --model=4 -r 127.0.0.1:12345'
 alias pcon_ke6afe='lrigctl V VFOA F 145630000 M PKTFM 0; pat connect ax25:///KE6AFE-10'
 alias pcon_n6zx='lrigctl V VFOA F 145630000 M PKTFM 0; pat connect ax25:///N6ZX-10'
 
-alias bbs_n0ary='lrigctl V VFOA F 145090000 M PKTFM 0; axcall sm0 n0ary-1'
+alias bbs_n0ary='lrigctl V VFOA F 145090000 M PKTFM 0'
 
 alias ardopen='lrigctl V VFOA F 007102200 M USB 0'
 alias ardop='/usr/local/bin/piardopc 8515 plughw:3,0 plughw:3,0'
 
 alias aprsen='lrigctl V VFOA F 144390000 M PKTFM 0'
 alias modepkt='lrigctl V VFOA M PKTFM 0'
+alias modefm='lrigctl V VFOA M FM 0'
 alias modeusb='lrigctl V VFOA M USB 0'
+alias modelsb='lrigctl V VFOA M LSB 0'
+
+alias smodepkt='rigctl -m 2 -r 127.0.0.1:4532 V VFOA M PKTFM 0'
+alias smodefm='rigctl -m 2 -r 127.0.0.1:4532 V VFOA M FM 0'
+alias smodelsb='rigctl -m 2 -r 127.0.0.1:4532 V VFOA M LSB 0'
+alias smodeusb='rigctl -m 2 -r 127.0.0.1:4532 V VFOA M USB 0'
+alias smodecw='rigctl -m 2 -r 127.0.0.1:4532 V VFOA M CW 0'
 
 alias gt='GridTracker --enable-speech-dispatcher'
 
-alias yaac='java -jar /home/ryan/yaac/YAAC.jar'
-
 alias audio='pax11publish -r'
-
+alias python='/usr/bin/python3'
